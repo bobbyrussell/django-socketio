214b5645c76cd5ca1255c83082f330c2a6519aba
Bobby Russell <bobbyrussell@gmail.com>
2015-04-01 22:14:43 -0400

    Server Now Emit Number of Clients Connected
    The server will now emit the number of total clients connected upon
    connection

--- socketdjango/nodejs/server.js
+++ socketdjango/nodejs/server.js
@@ -6,13 +6,42 @@ http.listen(3000, function() {
   console.log('listening on *:3000');
 });
 
+var users = 0;
+
 io.on('connection', function(socket) {
+  var message;
+
+  users += 1;
   console.log('a user connected');
+
+  if (users == 1) {
+    message = 'There is  1 user online';
+  } else {
+    message = 'There are ' + users + ' users online';
+  }
   socket.emit('server', {message: 'Welcome to the chat!'});
+  socket.emit('server', {message: message});
+
   socket.on('chat', function(message) {
     socket.broadcast.emit('chat', message);
   });
+
   socket.on('entered', function(user) {
-    socket.broadcast.emit('server', {message: user + ' has entered the chat!'});
+    socket.broadcast.emit('server',
+                          {message: user + ' user has entered the chat!'});
+  });
+
+  socket.on('disconnect', function() {
+    var message;
+
+    console.log('a user disconnected');
+    users -= 1;
+
+    if (users == 1) {
+      message = 'There is 1 user online';
+    } else {
+      message = 'There are ' + users + ' users online';
+    }
+    socket.emit('server', {message: message});
   });
 });
